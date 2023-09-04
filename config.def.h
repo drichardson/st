b6845ec6edaf1c0c82c0f0886f9b97e6bfa81bc6
Doug Richardson <git@rekt.email>
2023-09-03 21:18:34 -0700

    try some new fonts

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:size=11:antialias=true:autohint=true";
+//static char *font = "Source Code Pro:size=12:antialias=true:autohint=true";
+static char *font = "Cascadia Code:size=14";
 static int borderpx = 2;
 
 /*
